f6a150b111df79a021338d0bb77ca7635374fe6a
sajeda <dhadhasajeda24@gmail.com>
2023-04-09 16:22:11 +0300

    Add project files.

---
+++ IdentityDamra/Data/AppdDbContext.cs
@@ -0,0 +1,22 @@
+﻿using IdentityDamra.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDamra.Data
+{
+    public class AppdDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppdDbContext(DbContextOptions<AppdDbContext> Option) : base(Option)
+        {
+
+        }
+
+
+
+    }
+    
+}
---
+++ IdentityDamra/Models/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDamra.Models.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+
+    }
+}
